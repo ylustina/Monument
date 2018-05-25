bcf8ab3d4bb15fc145ff38fd7b53c13c5a32a342
Yana Lustina <yana.lustina@gmail.com>
2018-05-25 14:45:33 -0500

    Add commands

---
+++ Commands - Task 1.py
@@ -0,0 +1,18 @@
+from  subprocess import Popen, PIPE
+
+
+
+
+commands = [
+    'sleep 3',
+    'ls -l /',
+    'find /',
+    'sleep 4',
+    'find /usr',
+    'date',
+    'sleep 5',
+    'uptime'
+]
+
+
+
